954477fa25b786584297b7481cae291fd24177ac
Jonas Svensson <jonas.svensson@midsummer.se>
2023-10-30 13:54:32 +0100

    Install mongoose to talk to mongo db and connect to db.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Use the container name "mongo" to talk to it instead of hardcoding IP adress since that can change between restarts.
+// Docker uses DNS to translate beetwen name and IP. 
+mongoose.connect("mongodb://jonas:1234@mongo/?authSource=admin")
+    .then(()=>console.log("Succesfullyy connected to DB"))
+    .catch(((error)=>console.log(error)));
 
 app.get("/", (req, res)=>{
     res.send("<h2>Hi There!!</h2>");
