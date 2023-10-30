d5780bf52a3f09615849894ce6a3196b9419991c
Jonas Svensson <jonas.svensson@midsummer.se>
2023-10-30 16:52:54 +0100

    Added config file for env variables. Added env variables to the docker compose file.

---
+++ config/config.js
@@ -0,0 +1,9 @@
+// A variable that holds all our environment variables
+module.exports = {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_PORT: process.env.MONGO_PORT || 27017,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD
+}
+
+
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config.js");
 
 const app = express();
 
 // Use the container name "mongo" to talk to it instead of hardcoding IP adress since that can change between restarts.
 // Docker uses DNS to translate beetwen name and IP. 
-mongoose.connect("mongodb://jonas:1234@mongo/?authSource=admin")
-    .then(()=>console.log("Succesfullyy connected to DB"))
-    .catch(((error)=>console.log(error)));
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose
+    .connect(mongoURL)
+    .then(()=>console.log(`User ${MONGO_USER} succesfullyy connected to DB`))
+    .catch(((error)=>{
+        console.log("User: ", MONGO_USER);
+        console.log(error);
+    }));
 
 app.get("/", (req, res)=>{
     res.send("<h2>Hi There!!</h2>");
