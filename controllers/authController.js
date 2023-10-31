7ec66c058c7ed08bf96686128779ce644c958ac7
jonassvensson <jonas.svensson@midsummer.se>
2023-10-31 12:22:53 +0100

    Added login.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,8 @@ const bcrypt = require("bcryptjs");
 
 exports.signUp = async (req, res) => {
     const {username, password} = req.body;
-    const hashpassword = await bcrypt.hash(password, 12);
     try {
+        const hashpassword = await bcrypt.hash(password, 12);
         const newUser = await User.create({
             username, 
             password: hashpassword,
@@ -16,6 +16,41 @@ exports.signUp = async (req, res) => {
             }
         });
 
+    } catch(error){
+        res.status().json({
+            status: "fail",
+        });
+    }
+}
+
+exports.login = async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const user = await User.findOne({username});
+
+        if (!user) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'user not found',
+            });            
+        }
+
+        const isCorrect = await bcrypt.compare(password, user.password);
+
+        if (isCorrect) {
+            res.status(200).json(
+                {
+                    status: 'success'
+                }
+            );
+        } else {
+            res.status(400).json(
+                {
+                    status: 'fail',
+                    message: 'incorrect username or password',
+                }
+            );
+        }
     } catch(error){
         res.status().json({
             status: "fail",
